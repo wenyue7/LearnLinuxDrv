48b12e1572beb1771262271982dcec228f53dd92
wenyue7 <872648180@qq.com>
2023-07-12 23:32:30 +0800

    cdev: add learn demo

---
+++ 2.char_dev_drv/cdev/mDemo/user_space_test.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+    > File Name: user_space_test.c
+    > Author: LiHongjin
+    > Mail: 872648180@qq.com 
+    > Created Time: 2023年07月12日 星期三 23时10分16秒
+ ************************************************************************/
+
+#include <stdio.h>
+
+int main(int argc, char *argv, char *envp[])
+{
+    return 0;
+}
